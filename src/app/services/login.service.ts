23f5e94486d1d382f7d3834712bf75469d41db48
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-10 22:23:55 -0500

    chore: update API URI in login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   
   token = AppToken.getInstance();
 
-  API_URI = 'https://backend-sena-app-production.up.railway.app/api';
+  API_URI = 'https://backend-sena-app-production.up.railway.app/login';
 
   constructor(private http: HttpClient, private router: Router) { }
 
